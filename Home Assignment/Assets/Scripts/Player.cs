beac0461d220da70893f7c61350407bc2f9d3286
dcini31 <73096398+dcini31@users.noreply.github.com>
2021-01-18 15:40:15 +0100

    collider

---
+++ Home Assignment/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 15f;
+
+    float xMin, xMax;
+    float padding = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setUpMoveBounderies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();       
+    }
+
+    private float AddNumbers(float n1, float n2, float n3)
+    {
+        return n1 + n2;
+    }
+
+    private void setUpMoveBounderies()
+    {
+        //setup of bounderies of movement according to camera
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0.09f, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(0.95f, 0, 0)).x - padding;
+    }
+
+    private void Move()
+    {
+        //deltaX is the change of the x-axis of the player 
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPos= current position on x + differece moved in x-axis
+        var newXPos = transform.position.x + deltaX;
+
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        //moves the Player on the x-axis
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+    }
+}
+
+
---
+++ Home Assignment/Assets/Scripts/Shredder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        print(otherObject.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        //when laser hits shredder it will be destroyed
+        Destroy(otherObject.gameObject);
+    }
+
+}
